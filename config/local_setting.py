2a921d5afc4312e967bb4edf39902c03aa5821a3
xwp <xwp_fullstack@163.com>
2018-12-22 18:11:43 +0800

    mvc

---
+++ config/local_setting.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+DEBUG = True
+SQLALCHEMY_ECHO = True
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123456@127.0.0.1/flaskPro?charset=utf8mb4'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENCODING = "utf8mb4"
