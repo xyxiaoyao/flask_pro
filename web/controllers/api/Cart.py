c6d90358869ecdab3a7c8018e516c0b787c4145f
1127162300@qq.com <123@qq.com>
2019-03-01 18:05:50 +0800

    购物车商品删除

--- web/controllers/api/Cart.py
+++ web/controllers/api/Cart.py
@@ -1,9 +1,41 @@
+from common.libs.UrlManager import UrlManager
 from common.models.member.MemberCart import MemberCart
 from web.controllers.api import route_api
-from flask import request,g,jsonify
+from flask import request, g, jsonify, json
 from common.models.food.Food import Food
+from common.libs.Helper import selectFilterObj,getDictFilterField
 from common.libs.member.CartService import CartService
+from application import app, db
 
+
+@route_api.route("/cart/index")
+def cartIndex():
+    resp = {'code': 200, 'msg': '添加购物车成功~', 'data': {}}
+    member_info = g.member_info
+    if not member_info:
+        resp['code'] = -1
+        resp['msg'] = "未登录~~"
+        return jsonify(resp)
+
+    cart_list = MemberCart.query.filter_by(member_id = member_info.id).all()
+    data_cart_list = []
+    if cart_list:
+        food_ids = selectFilterObj(cart_list,'food_id')
+        food_map = getDictFilterField(Food, Food.id, "id", food_ids)
+        for item in cart_list:
+            tmp_food_info = food_map[item.food_id]
+            tmp_data = {
+                "id": item.id,
+                "number": item.quantity,
+                "food_id": item.food_id,
+                "name": tmp_food_info.name,
+                "price": str(tmp_food_info.price),
+                "pic_url": UrlManager.buildImageUrl(tmp_food_info.main_image),
+                "active": True
+            }
+            data_cart_list.append(tmp_data)
+    resp['data']['list'] = data_cart_list
+    return jsonify(resp)
 
 
 @route_api.route("/cart/set", methods=["POST"])
@@ -18,11 +50,12 @@ def setCart():
         return jsonify(resp)
 
     member_info = g.member_info
+    cart_number = 0
     if not member_info:
         resp['code'] = -1
         resp['msg'] = "添加购物车失败-2~~"
         return jsonify(resp)
-
+    cart_number = MemberCart.query.filter_by(member_id=member_info.id).count()
     food_info = Food.query.filter_by( id =  food_id ).first()
     if not food_info:
         resp['code'] = -1
@@ -40,4 +73,26 @@ def setCart():
         resp['msg'] = "添加购物车失败-4~~"
         return jsonify(resp)
 
+    resp['data']['cart_number'] = int(cart_number)+1
+    return jsonify(resp)
+@route_api.route('/cart/del',methods=['POST'])
+def delCart():
+    resp = {'code': 200, 'msg': '操作成功~', 'data': {}}
+    req = request.values
+    params_goods = req['goods'] if 'goods' in req else None
+    new_goods = []
+    if params_goods:
+        items = json.loads(params_goods)
+    if not items or len(items)<1:
+        return jsonify(resp)
+    member_info = g.member_info
+    if not member_info:
+        resp['code'] = -1
+        resp['msg'] = "删除购物车失败-1~~"
+        return jsonify(resp)
+    ret = CartService.deleteItem(member_id=member_info.id,items=items)
+    if not ret:
+        resp['code'] = -1
+        resp['msg'] = "删除购物车失败-2~~"
+        return jsonify(resp)
     return jsonify(resp)
